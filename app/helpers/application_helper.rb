ac9165b1b221627d8a389350635c5472acfaa06f
Elise Adams <neuman.elise@gmail.com>
2017-05-25 11:18:53 -0600

    Updated homepage flow for admins and users.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,18 @@
 module ApplicationHelper
+  def admin_types
+    ['AdminUser']
+  end
+
   def active?(path)
-      "active" if current_page?(path)
-    end
+    "active" if current_page?(path)
+  end
 
   def status_label status
     status_span_generator status
   end
 
   private
-
+    
     def status_span_generator status
       case status
       when 'submitted'
